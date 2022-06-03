ce1fd5b339bb018a758023f31e9934ff1a10cdac
Juraj Bergman <jurajbergman@gmail.com>
2022-06-03 15:29:49 +0200

    Update fig_3_git.R

--- scripts/fig_3_git.R
+++ scripts/fig_3_git.R
@@ -21,19 +21,19 @@ p1 <- ggplot(data, aes(x = gcDiff, y=perYearRates/1000000,col=ax)) + geom_point(
   ylab("Divergence rate (PY)") + xlab(expression("GC* - GC (current)"))+
   theme(legend.title = element_blank(), legend.position = "none",legend.box.background = element_rect(colour = "black"),
         legend.margin =margin(r=5,l=0,t=-5,b=0))
+p1
 
 p2 <- ggplot(data, aes(x = gcDiff, y=tstv,col=ax)) + geom_point() +theme_bw()+ scale_color_manual(values = c("black", "#FFD700")) +
   ylab("ts:tv") + xlab(expression("GC* - GC (current)"))+
   theme(legend.title = element_blank(), legend.position = "none",legend.box.background = element_rect(colour = "black"),
         legend.margin =margin(r=5,l=0,t=-5,b=0))
+p2
 
 p3 <- ggplot(data, aes(x = gcDiff, y=len/1000000,col=axr)) + geom_point() +theme_bw()+ scale_color_manual(values = c("black", "red","#FFD700")) +
   ylab("Chromosome length (Mb)") + xlab("GC* - GC (current)")+
   theme(legend.title = element_blank(), legend.position = c(0.87,0.2),legend.box.background = element_rect(colour = "black"),
         legend.margin =margin(r=5,l=0,t=-5,b=0))
+p3
 
 p4 <-ggcorrplot(corr, p.mat = p.mat,type = "upper",lab = TRUE, colors = c("#377eb8", "white", "#e41a1c"))
-
-tt <- plot_grid(p1, p2, p3, p4, ncol = 2, align="h", rel_widths = c(1,1), labels = c("A", "B", "C", "D"))
-
-ggsave("fig3.png", tt, width=20, height=16, units = "cm")
+p4
